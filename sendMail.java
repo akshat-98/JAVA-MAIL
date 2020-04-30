8910b299c3ac97d0a8dbe85aaf911763f2355b2d
akshat-98 <villa.akshat98@gmail.com>
2020-04-30 18:48:22 +0530

    Add files via upload

---
+++ sendMail.java
@@ -0,0 +1,52 @@
+package mail;
+
+import java.security.Security;
+import java.util.Date;
+import java.util.Properties;
+
+import javax.mail.Authenticator;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+public class sendMail {
+	
+	public static void main(String[] args) throws Exception {
+		// TODO code application logic here
+		Security.addProvider(new com.sun.net.ssl.internal.ssl.Provider());
+		final String SSL_FACTORY = "javax.net.ssl.SSLSocketFactory";
+		// Get a Properties object
+		Properties props = System.getProperties();
+		props.setProperty("mail.smtp.host", "smtp.gmail.com");
+		props.setProperty("mail.smtp.socketFactory.class", SSL_FACTORY);
+		props.setProperty("mail.smtp.socketFactory.fallback", "false");
+		props.setProperty("mail.smtp.port", "465");
+		props.setProperty("mail.smtp.socketFactory.port", "465");
+		props.put("mail.smtp.auth", "true");
+		final String username = "xxxxxx"; //add your username without @gmail.com
+		final String password = "xxxxxx"; //the password of your email id.
+		Session session = Session.getDefaultInstance(props, new Authenticator() {
+			protected PasswordAuthentication getPasswordAuthentication() {
+				return new PasswordAuthentication(username, password);
+			}
+		});
+
+		// -- Create a new message --
+		Message msg = new MimeMessage(session);
+
+		// -- Set the FROM and TO fields --
+		msg.setFrom(new InternetAddress(username + "@gmail.com"));
+		msg.setRecipients(Message.RecipientType.TO, InternetAddress.parse("sendToAddress", false)); //replace sendToAddress with the email id to which you wish to send your email.
+		msg.setSubject("Hello"); //Change your subject
+		msg.setText("How are you"); //Change your email text
+		msg.setSentDate(new Date());
+		Transport.send(msg);
+
+		System.out.println("Message sent.");
+	}
+}
